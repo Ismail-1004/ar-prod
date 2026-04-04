e66402cc0dfbe2540a9ddd9fa1c951c721132b88
herheulidzeismail980@gmail.com <herheulidzeismail980@gmail.com>
2026-04-04 15:43:30 +0500

    size update

--- src/pages/AR.jsx
+++ src/pages/AR.jsx
@@ -78,8 +78,8 @@ function ARPage() {
 
                     const box = new THREE.Box3().setFromObject(model)
                     const size = box.getSize(new THREE.Vector3())
-                    const maxDim = Math.max(size.x, size.y, size.z)
-                    const scale = 0.75 / maxDim
+                    // const maxDim = Math.max(size.x, size.y, size.z)
+                    const scale = 1
                     model.scale.setScalar(scale)
 
                     const center = box.getCenter(new THREE.Vector3())
